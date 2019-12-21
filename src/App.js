3501ae2edaa6f0dd1a15962af3df43b3d0c6dcb8
Aleksandr Yuryev <veyruyskela@gmail.com>
2019-12-21 10:41:35 +0400

    qwe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './reset.scss'
 import Header from "./components/HomePage/Header";
+import Menu from "./components/HomePage/Menu";
 import CreateNewPost from "./components/CreateNewPost";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import PDP from "./components/PDP";
+import Menu from "../../Final-Project-header/src/components/HomePage/Menu";
 
 function App() {
     return (
@@ -13,13 +15,16 @@ function App() {
                 <Switch>
                     <Route path='/' exact>
                     <Header/>
+                    <Menu/>
                     </Route>
                     <Route path='/new'>
                         <Header/>
+                        <Menu/>
                         <CreateNewPost/>
                     </Route>
                     <Route path='/PDP'>
                         <Header/>
+                        <Menu/>
                         <PDP/>
                     </Route>
                 </Switch>
